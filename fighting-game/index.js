b0814787ed888f28f77c234eb05d11a740059819
annalhq <155549764+annalhq@users.noreply.github.com>
2024-12-28 15:54:49 +0530

    completed project setup

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -1,3 +1,9 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d'); // 2D rendering context
-  
+
+// Set the canvas size (16:9 aspect ratio)
+canvas.width = 1024;
+canvas.height = 576;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
+ctx.fillRect(0, 0, canvas.width, canvas.height); // fillRect(x, y, width, height) draws a rectangle whose starting point is at (x, y) and whose width and height
