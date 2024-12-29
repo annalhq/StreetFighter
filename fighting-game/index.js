fab2e230fc885710a2a7b3b5381ad846054c88aa
annalhq <annalhqshaikh@gmail.com>
2024-12-30 00:03:13 +0530

    player sprite template completed

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -8,22 +8,34 @@ canvas.height = 576;
 // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
 ctx.fillRect(0, 0, canvas.width, canvas.height); // fillRect(x, y, width, height) draws a rectangle whose starting point is at (x, y) and whose width and height
 
+const gravity = 0.2
+
 class Sprite {
-   constructor(position, velocity){
+   constructor({ position, velocity }) {
      this.position = position;
-     this.velocity = velcoity;
+     this.velocity = velocity;
+     this.height = 150;
    }
   
-  draw(){
+  draw() {
     ctx.fillStyle = 'red'
-    ctx.fillRect(this.position.x, this.position.y, 50, 150)
+    ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+    } else this.velocity.y += gravity 
   }
 }
 
 const player = new Sprite({
   position:{
-  x:0,
-  y:0
+    x:0,
+    y:0
   },
   velocity:{
     x:0,
@@ -31,18 +43,23 @@ const player = new Sprite({
   }
 })
 
-player.draw()
-
 const enemy = new Sprite({
-  x:400,
-  y:100
+  position:{
+    x:400,
+    y:100
+  },
+  velocity:{
+    x:0,
+    y:0
+  }
 })
 
-enemy.draw()
-
-function animate(){
-  window.requestAnimationFrame
-
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player.update()
+  enemy.update()
 }
 
-animate()
+animate();
