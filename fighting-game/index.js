cea88b80c340029284a0d373ba1bc7a8ab63a62f
annalhq <annalhqshaikh@gmail.com>
2024-12-29 13:59:53 +0530

    updated: sprite constructor

--- fighting-game/index.js
+++ fighting-game/index.js
@@ -9,9 +9,40 @@ canvas.height = 576;
 ctx.fillRect(0, 0, canvas.width, canvas.height); // fillRect(x, y, width, height) draws a rectangle whose starting point is at (x, y) and whose width and height
 
 class Sprite {
-   constructor(position){
+   constructor(position, velocity){
      this.position = position;
-   }  
+     this.velocity = velcoity;
+   }
+  
+  draw(){
+    ctx.fillStyle = 'red'
+    ctx.fillRect(this.position.x, this.position.y, 50, 150)
+  }
 }
 
-const player = new Sprite({})
+const player = new Sprite({
+  position:{
+  x:0,
+  y:0
+  },
+  velocity:{
+    x:0,
+    y:0
+  }
+})
+
+player.draw()
+
+const enemy = new Sprite({
+  x:400,
+  y:100
+})
+
+enemy.draw()
+
+function animate(){
+  window.requestAnimationFrame
+
+}
+
+animate()
