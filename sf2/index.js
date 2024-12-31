5341a1f914b3b21f7788b2923ec85740f2e9d28f
annalhq <155549764+annalhq@users.noreply.github.com>
2024-12-31 20:12:22 +0530

    added: constraints to sprites

--- sf2/index.js
+++ sf2/index.js
@@ -7,11 +7,10 @@ window.onload = function () {
     const canvasComp = document.querySelector('canvas');
     const context = canvasComp.getContext('2d');
 
-    const ken = document.querySelector('img')
-
     canvasComp.width = GameViewport.WIDTH;
     canvasComp.height = GameViewport.HEIGHT;
 
+    // cross lines for testing
     context.strokeStyle = 'yellow';
     context.moveTo(0, 0);
     context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
@@ -19,17 +18,34 @@ window.onload = function () {
     context.lineTo(0, GameViewport.HEIGHT);
     context.stroke();
 
-    context.drawImage(ken, 0, 0 );
+    // characters image
+    const ken = document.querySelector('img')
+
+    const position = {
+        x: 0,
+        y: 0,
+    }
+
+    let velocity = 3;
 
     // fps logic
     function frame() {
+        position.x += velocity;
+
+        if (position.x > GameViewport.WIDTH - ken.width || position.x < 0) {
+            velocity = -velocity;
+        }
+
+        context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT)
+
         context.strokeStyle = 'yellow';
         context.moveTo(0, 0);
         context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
         context.moveTo(GameViewport.WIDTH, 0);
         context.lineTo(0, GameViewport.HEIGHT);
         context.stroke();
-        context.drawImage(ken, 0, 0 );
+
+        context.drawImage(ken, position.x, position.y);
         window.requestAnimationFrame(frame);
     }
 
