88609d5d1331565ea11874e7f688592f96bcb9b5
annalhq <annalhqshaikh@gmail.com>
2024-12-31 17:06:11 +0530

    minor changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game Development
+
+Personal game development projects.
+
+Thanks to the tutorials by Anthony Sherratt and Chris Lis.
--- sf2/index.js
+++ sf2/index.js
@@ -1,16 +1,19 @@
 const GameViewport = {
     WIDTH: 384,
     HEIGHT: 224,
-    SCALE: 4,
 }
 
 window.onload = function () {
     const canvasComp = document.querySelector('canvas');
     const context = canvasComp.getContext('2d');
 
-    canvasComp.width = GameViewport.WIDTH * GameViewport.SCALE;
-    canvasComp.height = GameViewport.HEIGHT * GameViewport.SCALE;
+    canvasComp.width = GameViewport.WIDTH;
+    canvasComp.height = GameViewport.HEIGHT;
 
-    canvasComp.style.width = `${GameViewport.WIDTH*GameViewport.SCALE}px`;
-    canvasComp.style.height = `${GameViewport.HEIGHT*GameViewport.SCALE}px`;
+    context.strokeStyle = 'yellow';
+    context.moveTo(0, 0);
+    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+    context.moveTo(GameViewport.WIDTH, 0);
+    context.lineTo(0, GameViewport.HEIGHT);
+    context.stroke();
 }
