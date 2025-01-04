113f299a6b939876b8fd30e037117ad0ad14caf8
annalhq <annalhqshaikh@gmail.com>
2025-01-04 22:22:41 +0530

    fix: movement select form bug fixed

--- src/entities/fighters/Fighter.js
+++ src/entities/fighters/Fighter.js
@@ -33,7 +33,7 @@ export class Fighter {
     this.states[this.currentState].init();
   }
 
-  handleWalkForwardInit() {
+  handleWalkForwardInit( ) {
     this.velocity = 150 * this.direction;
   }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,57 @@ import { Ryu } from './entities/fighters/Ryu.js';
 import { Stage } from './entities/Stage.js';
 import { FpsCounter } from './entities/fpsCounter.js';
 import { STAGE_FLOOR } from './constants/stage.js';
-import { FighterDirection } from './constants/fighter.js';
+import { FighterDirection, FighterState } from './constants/fighter.js';
+
+function populateMoveDropdown(){
+  const dropdown = document.getElementById('stage-dropdown');
+
+  Object.entries(FighterState).forEach(([, value])=> {
+    const option = document.createElement('option');
+    option.setAttribute('value', value);
+    option.innerText = value;
+    dropdown.appendChild(option);
+  });
+}
+
+
+function handleFormSubmit(event, fighters) {
+  event.preventDefault();
+
+  const selectedCheckbox = Array.from(
+    event.target.querySelectorAll('input[type="checkbox"]:checked')
+  ).map(checkbox => checkbox.value);
+
+  const select = event.target.querySelector('select');
+  const selectedValue = select.value;
+
+  fighters.forEach(fighter => {
+    if (selectedCheckbox.includes(fighter.name)) {
+      fighter.changeState(selectedValue);
+    }
+  });
+
+
+
+}
 
 window.addEventListener('load', function () {
+
+  populateMoveDropdown();
+
   const canvasComp = document.querySelector('canvas');
   const context = canvasComp.getContext('2d');
   context.imageSmoothingEnabled = false;
 
-  const entities = [
-    new Stage(),
+  const fighters = [
     new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
     new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
+    
+  ];
+  
+  const entities = [
+    new Stage(),
+    ...fighters,
     new FpsCounter(),
   ];
 
@@ -41,5 +81,7 @@ window.addEventListener('load', function () {
     }
   }
 
+  this.document.addEventListener('submit', event => handleFormSubmit(event, fighters));
+  
   window.requestAnimationFrame(frame);
 });
